d823517b7ad3d426cdeac851ede603a47771da50
WiliamNasc <wilnasc990s3@gmail.com>
2023-10-26 19:57:03 -0300

    Adicionando base do projeto

---
+++ src/main/java/br/com/fiap/communitymanagement/CommunityManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.communitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/communitymanagement/CommunityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.communitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
